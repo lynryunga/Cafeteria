ff250ec8506cc6930d508993392339e4a7db2871
Leynner Yunga <lynryunga@gmail.com>
2023-01-18 11:40:41 -0500

    Minificando imagenes a formatos avif, webp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
-const autoprefixer = require('autoprefixer');
 const { src, dest, watch, series, parallel } = require('gulp');
+
+//CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
-// const autoprefixer =  require('autoprefixer');
+const autoprefixer =  require('autoprefixer');
+
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css( done ) {
   //compilar SASS
@@ -16,13 +22,41 @@ function css( done ) {
   done();
 }
 
+function imagenes() {
+  return src( 'src/img/**/*' )
+  .pipe ( imagemin({ optimizationLevel: 3 }) )
+  .pipe( dest('build/img') )
+}
+
+function versionWebp() {
+  const opciones = {
+    quality: 50
+  }
+  return src( 'src/img/**/*.{png,jpg}' )
+    .pipe( webp( opciones ) )
+    .pipe( dest('build/img') )
+}
+
+function versionAvif() {
+  const opciones = {
+    quality: 50
+  }
+  return src( 'src/img/**/*.{png,jpg}' )
+    .pipe( avif( opciones ) )
+    .pipe( dest('build/img') )
+}
+
 function dev () {
-  watch( 'src/scss/**/*.scss', css )
+  watch( 'src/scss/**/*.scss', css );
+  watch( 'src/img/**/*', imagenes )
 }
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev );
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( imagenes, versionWebp, versionAvif, css, dev );
 
 //Series: Se inicia una tarea y hasta que finaliza inicial la siguiente
 //parallel: Todas inician al mismo tiempo
